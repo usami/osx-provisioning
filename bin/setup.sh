74e3ce2159210a0ed2dbcde235a5299e7a8fad1f
Yu Usami <yusmi05@gmail.com>
2016-04-12 18:30:34 +0900

    Add setup script

---
+++ bin/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+echo "Setup Ansible"
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ansible git
+
+echo "Clone playbooks"
+git clone https://github.com/usami/osx-provisioning ~/src/osx-provisioning
+
+echo "Start provisioning"
+
+cd ~/src/osx-provisioning
+HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook osx.yml
